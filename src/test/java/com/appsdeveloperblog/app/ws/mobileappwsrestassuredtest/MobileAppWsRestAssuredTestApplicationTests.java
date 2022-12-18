9e57e83d06a606dc5f3489247f27040536e1e4e3
Sean Mayer <part-things.0p@icloud.com>
2022-12-18 16:47:20 +0000

    Initilised spring boot project

---
+++ src/test/java/com/appsdeveloperblog/app/ws/mobileappwsrestassuredtest/MobileAppWsRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.mobileappwsrestassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsRestAssuredTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
