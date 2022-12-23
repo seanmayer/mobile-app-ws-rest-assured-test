c4b56435fcc3c4ae3cfb83c79cf45d890ad6d633
Sean Mayer <apart-things.0p@icloud.com>
2022-12-23 18:04:42 +0000

    Integration test - Added GetLogin test

--- src/test/java/com/appsdeveloperblog/app/ws/mobileappwsrestassuredtest/UsersWebServiceEndpointTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/mobileappwsrestassuredtest/UsersWebServiceEndpointTest.java
@@ -1,12 +1,20 @@
 package com.appsdeveloperblog.app.ws.mobileappwsrestassuredtest;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UsersWebServiceEndpointTest {
 
   private final String CONTEXT_PATH = "/mobile-app-ws";
+  private final String EMAIL_ADDRESS = "test@test.com";
+  private final String JSON = "application/json";
 
   @BeforeEach
   void setUp() throws Exception {
@@ -15,5 +23,27 @@ public class UsersWebServiceEndpointTest {
   }
 
   @Test
-  void testGetLogin() {}
+  void testGetLogin() {
+    Map<String, String> loginDetails = new HashMap<>();
+    loginDetails.put("email", EMAIL_ADDRESS);
+    loginDetails.put("password", "123456");
+
+    Response response = RestAssured
+      .given()
+      .contentType(JSON)
+      .accept(JSON)
+      .body(loginDetails)
+      .when()
+      .post(CONTEXT_PATH + "/users/login")
+      .then()
+      .statusCode(200)
+      .extract()
+      .response();
+
+      String authorizationHeader = response.header("Authorization");
+      String userId = response.header("UserID");
+
+      assertNotNull(authorizationHeader);
+      assertNotNull(userId);
+  }
 }
