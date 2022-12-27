ba04da2b0dd20b09c2ce861e13ddea8705db6cb7
Sean Mayer <apart-things.0p@icloud.com>
2022-12-27 17:53:03 +0000

    Integration Tests - getUserDetails

--- src/test/java/com/appsdeveloperblog/app/ws/mobileappwsrestassuredtest/UsersWebServiceEndpointTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/mobileappwsrestassuredtest/UsersWebServiceEndpointTest.java
@@ -6,15 +6,21 @@ import io.restassured.response.Response;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class UsersWebServiceEndpointTest {
 
   private final String CONTEXT_PATH = "/mobile-app-ws";
-  private final String EMAIL_ADDRESS = "test@test.com";
+  private final String EMAIL_ADDRESS = "contact.sean.mayer@gmail.com";
   private final String JSON = "application/json";
+  private static String authorizationHeader;
+  private static String userId;
 
   @BeforeEach
   void setUp() throws Exception {
@@ -22,8 +28,11 @@ public class UsersWebServiceEndpointTest {
     RestAssured.port = 8080;
   }
 
+  /*
+   * Test user login
+   */
   @Test
-  void testGetLogin() {
+  void a() {
     Map<String, String> loginDetails = new HashMap<>();
     loginDetails.put("email", EMAIL_ADDRESS);
     loginDetails.put("password", "123456");
@@ -40,10 +49,45 @@ public class UsersWebServiceEndpointTest {
       .extract()
       .response();
 
-      String authorizationHeader = response.header("Authorization");
-      String userId = response.header("UserID");
+      authorizationHeader = response.header("Authorization");
+      userId = response.header("UserID");
 
       assertNotNull(authorizationHeader);
       assertNotNull(userId);
+  }
+
+  /*
+   * Test get user details
+   */
+  @Test
+  void b() {
+    Response response = RestAssured
+      .given()
+      .pathParam("id", userId)
+      .header("Authorization", authorizationHeader)
+      .accept(JSON)
+      .when()
+      .get(CONTEXT_PATH + "/users/{id}")
+      .then()
+      .statusCode(200)
+      .contentType(JSON)
+      .extract()
+      .response();
+
+      String userPublicId = response.jsonPath().getString("userId");
+      String email = response.jsonPath().getString("email");
+      String firstName = response.jsonPath().getString("firstName");
+      String lastName = response.jsonPath().getString("lastName");
+      List<Map<String, String>> addresses = response.jsonPath().getList("addresses");
+      /* addressId currently null, this is a one to many relationship which can be invoked with /mobile-app-ws/users/{id}/addresses */
+      //String addressId = addresses.get(0).get("addressId");
+
+      assertNotNull(userPublicId);
+      assertNotNull(email);
+      assertNotNull(firstName);
+      assertNotNull(lastName);
+      //assertTrue(addresses.size() == 2);
+      //assertEquals(EMAIL_ADDRESS, email);
+      //assertTrue(addressId.length() == 30);
   }
 }
